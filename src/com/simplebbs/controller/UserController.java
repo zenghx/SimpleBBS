6ee025bf2574d4429e35cdfea0569035455136d5
Hongxin Zeng <zenghongx@gmail.com>
2019-12-20 22:13:21 +0800

    navbar+index+section

--- src/com/simplebbs/controller/UserController.java
+++ src/com/simplebbs/controller/UserController.java
@@ -3,7 +3,6 @@ package com.simplebbs.controller;
 import com.simplebbs.po.UserInfo;
 import com.simplebbs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
